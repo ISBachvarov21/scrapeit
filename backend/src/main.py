53faa6400fe6c661f33295f0528b5d0b481bc196
Ivaylo Bachvarov <ISBachvarov21@codingburgas.bg>
2024-09-09 22:37:41 +0300

    Finish scraping part

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,37 +1,37 @@
-# import pytesseract
-# import cv2
+import pytesseract
+import cv2
 import asyncio
 from pyppeteer import launch
 import bs4
 from fuzzysearch import find_near_matches
 
 async def main():
-    # # TODO: dynamic image reading and text extraction
-    # # TODO: should come after backend is implemented
-    # image = cv2.imread(r'images/ss3.png')
+    # TODO: dynamic image reading and text extraction
+    # TODO: should come after backend is implemented
+    # image = cv2.imread(r'images/ss.png')
+    image = cv2.imread(r'images/amazon-usb-c.png')
 
-    # # Convert the image to grayscale
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Convert the image to grayscale
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
-    # # HACK: hardcoded crop
+    # HACK: hardcoded crop
     # image = image[230:274, 58:272]
+    # image = image[92:112, 86:270]
 
-
-    # text = pytesseract.image_to_string(image, lang='eng+bul')
+    text = pytesseract.image_to_string(image, lang='eng')
     
     browser = await launch(headless=True, args=['--no-sandbox', '--headless', '--disable-gpu'])
     page = await browser.newPage()
     await page.setViewport({'width': 1920, 'height': 1080})
-    await page.goto('https://karclean.bg/', {'waitUntil': 'networkidle0'})
+    await page.goto('https://www.amazon.com/s?k=usb+c&crid=13MUUAA4E6QXG&sprefix=usb+%2Caps%2C220&ref=nb_sb_noss_2', {'waitUntil': 'networkidle0'})
+    await page.screenshot({'path': 'images/amazon-usb-c.png', 'fullPage': True})
     
     content = await page.content()
     
     soup = bs4.BeautifulSoup(content, 'html5lib')
     
-    # text = text.strip().split('\n')
+    text = text.strip().split('\n')
 
-    text = ["KARCHER WD 3 V-17/4/20"]
-    
     matches = []
     for subtext in text:
         matches = find_near_matches(subtext, soup.body.prettify(), max_l_dist=4) # type: ignore
@@ -40,40 +40,37 @@ async def main():
     
     matched_element = None
     for match in matches:
-        print(f'trying to match: {match.matched}')
         matched_element = soup.find(lambda tag: tag.string and match.matched in tag.string) # type: ignore
         if matched_element:
             break
     
     if not matched_element:
         print('No match found')
+        print('Text:', text)
         return
-    
-    print(f'Matched: {matched_element}')
     
     current_element = matched_element
     parent_tags = []
     
-    # NOTE: Changing this to more than 2 levels up breaks the CSS selector.
-    # TODO: Submit issue to soupsieve
-    for _ in range(2):
-        current_element = current_element.find_parent()
+    while current_element.parent.parent is not None:
+        current_element = current_element.parent
         if current_element:
             parent_tags.append(current_element.name)
         else:
             break
-    
-    print(f'Parent tags: {parent_tags}')
-    css_selector = f'{" ".join(parent_tags)} {matched_element.name}'
-    print(f'CSS selector: {css_selector}')
+        
+    # NOTE: parent hierarchy must be reversed when constructing the CSS selector
+    css_selector = f'{" > ".join(parent_tags[::-1])} > {matched_element.name}'
 
+    print(f'CSS selector: {css_selector}')
     # Find all elements that match this parent hierarchy
-    matching_elements = soup.css.select(css_selector)
+    matching_elements = soup.css.select(css_selector) # type: ignore
 
     print(f'Found {len(matching_elements)} matching elements')
-    # for element in matching_elements:
-    #     print(element.text)
-    
+    for element in matching_elements:
+        print(element.text)
+        
+    await browser.close()
 
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
